d3c8292b045b8a395d8e3533eb75820103ac57f2
Rohit Sirsat <rohitsirsat855@gmail.com>
2023-12-08 20:58:57 +0530

    data types

---
+++ practice/01_variables.js
@@ -0,0 +1,10 @@
+/* variables why not var
+because var creates an issue in block scope and functional scope
+*/
+const num = 123;
+let firstName = "rohit";
+var lastName = "sisat";
+cityName = "Akola"; // js allow it but prefer not to use
+let moNumber;
+
+console.table([num, firstName, lastName, cityName, moNumber]);
