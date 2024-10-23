901fe54848cb0468b1b1ac97b1eb5ee97fedaaa1
rohit <rohitsirsat855@gmail.com>
2024-10-23 13:51:40 +0530

    add todo with local storage

--- 09todo-localStorage/script.js
+++ 09todo-localStorage/script.js
@@ -1,23 +1,54 @@
-const todoInpudt = document.getElementById("todo-input");
-const addTaskButton = document.getElementById("add-task-btn");
-const todoList = document.getElementById("todo-list");
+document.addEventListener("DOMContentLoaded", () => {
+  const todoInput = document.getElementById("todo-input");
+  const addTaskButton = document.getElementById("add-task-btn");
+  const todoList = document.getElementById("todo-list");
 
-let tasks = [];
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-addTaskButton.addEventListener("click", (e) => {
-  const taskText = todoInpudt.value.trim();
-  if (taskText === "") return alert("add a task");
+  tasks.forEach((task) => renderTask(task));
 
-  const newTask = {
-    id: Date.now(),
-    text: taskText,
-    completed: false,
-  };
+  addTaskButton.addEventListener("click", () => {
+    const taskText = todoInput.value.trim();
+    if (taskText === "") return;
 
-  tasks.push(newTask);
-  todoInpudt.value = ""; // clear input
+    const newTask = {
+      id: Date.now(),
+      text: taskText,
+      completed: false,
+    };
+    tasks.push(newTask);
+    saveTasks();
+    renderTask(newTask);
+    todoInput.value = ""; //clear input
+    console.log(tasks);
+  });
 
-  console.log(tasks);
+  function renderTask(task) {
+    const li = document.createElement("li");
+    li.setAttribute("data-id", task.id);
+    if (task.completed) li.classList.add("completed");
+    li.innerHTML = `
+    <span>${task.text}</span>
+    <button>delete</button>
+    `;
+    li.addEventListener("click", (e) => {
+      if (e.target.tagName === "BUTTON") return;
+      task.completed = !task.completed;
+      li.classList.toggle("completed");
+      saveTasks();
+    });
+
+    li.querySelector("button").addEventListener("click", (e) => {
+      e.stopPropagation(); //prevent toggle from firing
+      tasks = tasks.filter((t) => t.id === task.id);
+      li.remove();
+      saveTasks();
+    });
+
+    todoList.appendChild(li);
+  }
+
+  function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
 });
-
-function saveTask() {}
