22af0eca1e8d10ecb8118be36b1b195e0acc59d3
rohit <rohitsirsat855@gmail.com>
2024-10-23 13:52:27 +0530

    add weather app

---
+++ 10weatherApp/practice.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cityInput = document.getElementById("city-input");
+  const getWeatherBtn = document.getElementById("get-weather-btn");
+  const weatherInfo = document.getElementById("weather-info");
+  const cityNameDisplay = document.getElementById("city-name");
+  const temperatureDisplay = document.getElementById("temperature");
+  const descriptionDisplay = document.getElementById("description");
+  const errorMessage = document.getElementById("error-message");
+
+  const API_KEY = "547e3c48b2c8cdb542863aff09fe4d6e"; //env variables
+});
